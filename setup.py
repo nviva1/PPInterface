eb901d1d58c2d7af4cc1cec386452f3d5fa92d22
nviva1 <n.ivanisenko@gmail.com>
2024-10-03 14:36:02 +0200

    update

--- setup.py
+++ setup.py
@@ -4,20 +4,6 @@ import subprocess
 import os
 import sys
 
-
-# Read dependencies from requirements.txt
-def read_requirements():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
-#class PostInstallCommand(install):
-#    """Post-installation for installing submodules."""
-#    def run(self):
-#        # Clone submodules
-#        subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
-#        # Continue with the installation
-#        install.run(self)
-
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     
@@ -43,27 +29,17 @@ class PostInstallCommand(install):
         else:
             print(f"'proteinmpnn_wrapper' directory not found at {submodule_dir}. Skipping installation.")
 
+# Helper function to read requirements.txt
+def read_requirements():
+    with open('requirements.txt') as req:
+        return req.read().splitlines()
+
 setup(
     name='PPInterface',
-    version='0.1.0',
-    description='TBA',
-    long_description=open('README.md').read(),  # Optional: use README as long description
-    long_description_content_type='text/markdown',
-    author='Nikita Ivanisenko',
-    author_email='n.ivanisenko@gmail.com',
-    url='https://github.com/username/your-repo',  # Replace with the actual repo URL
+    version='0.1',
     packages=find_packages(),
-    install_requires=read_requirements(),  # Load requirements from the file
-    classifiers=[
-        'Development Status :: 3 - Alpha',  # Adjust based on your project's status
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',  # Specify the minimum Python version
+    install_requires=read_requirements(),  # Automatically install packages from requirements.txt
     cmdclass={
         'install': PostInstallCommand,
-        }
+    },
 )
-
-
