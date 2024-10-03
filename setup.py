8029ca7aa54a522f2796cbc1a4a58bd3611ab918
nviva1 <n.ivanisenko@gmail.com>
2024-10-03 13:36:26 +0200

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess
+import os
 
 # Read dependencies from requirements.txt
 def read_requirements():
     with open('requirements.txt') as f:
         return f.read().splitlines()
+
+# Custom install command to handle recursive submodule Git installation
+class CustomInstallCommand(install):
+    def run(self):
+        # Clone the required repository with --recurse-submodules
+        subprocess.check_call(['git', 'clone', '--recurse-submodules', 
+                               'https://github.com/Croydon-Brixton/proteinmpnn_wrapper.git'])
+        # Change directory and install the repository
+        os.chdir('proteinmpnn_wrapper')
+        subprocess.check_call(['pip', 'install', '.'])
+        os.chdir('..')  # Return to the original directory
+
+        # Run the standard install process
+        install.run(self)
 
 setup(
     name='PPInterface',
@@ -13,7 +30,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Nikita Ivanisenko',
     author_email='n.ivanisenko@gmail.com',
-    url='https://github.com/username/your-repo',  # Replace with the actual repo URL
+    url='https://github.com/nviva1/PPInterface',  # Replace with the actual repo URL
     packages=find_packages(),
     install_requires=read_requirements(),  # Load requirements from the file
     classifiers=[
@@ -23,4 +40,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',  # Specify the minimum Python version
+    cmdclass={
+        'install': CustomInstallCommand,  # Use the custom install command
+    }
 )
