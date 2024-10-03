046ca9e926f1eef4c0713699ff4dbc3c29c3d8b2
nviva1 <n.ivanisenko@gmail.com>
2024-10-03 13:48:23 +0200

    update

--- setup.py
+++ setup.py
@@ -1,26 +1,33 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-import subprocess
-import os
 
 # Read dependencies from requirements.txt
 def read_requirements():
     with open('requirements.txt') as f:
         return f.read().splitlines()
 
-# Custom install command to handle recursive submodule Git installation
-class CustomInstallCommand(install):
-    def run(self):
-        # Clone the required repository with --recurse-submodules
-        subprocess.check_call(['git', 'clone', '--recurse-submodules', 
-                               'https://github.com/Croydon-Brixton/proteinmpnn_wrapper.git'])
-        # Change directory and install the repository
-        os.chdir('proteinmpnn_wrapper')
-        subprocess.check_call(['pip', 'install', '.'])
-        os.chdir('..')  # Return to the original directory
+#class PostInstallCommand(install):
+#    """Post-installation for installing submodules."""
+#    def run(self):
+#        # Clone submodules
+#        subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+#        # Continue with the installation
+#        install.run(self)
 
-        # Run the standard install process
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # First, run the normal install process
         install.run(self)
+        
+        # Then run your custom command
+        try:
+            # Change directory to 'proteinmpnn_wrapper'
+            os.chdir('proteinmpnn_wrapper')
+            # Install the package in that directory
+            subprocess.check_call(['pip', 'install', '.'])
+        except Exception as e:
+            print(f"Error installing proteinmpnn_wrapper: {e}")
 
 setup(
     name='PPInterface',
@@ -30,7 +37,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Nikita Ivanisenko',
     author_email='n.ivanisenko@gmail.com',
-    url='https://github.com/nviva1/PPInterface',  # Replace with the actual repo URL
+    url='https://github.com/username/your-repo',  # Replace with the actual repo URL
     packages=find_packages(),
     install_requires=read_requirements(),  # Load requirements from the file
     classifiers=[
@@ -40,7 +47,9 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',  # Specify the minimum Python version
-    cmdclass={
-        'install': CustomInstallCommand,  # Use the custom install command
-    }
+#    cmdclass={
+#        'install': PostInstallCommand,
+#        }
 )
+
+
