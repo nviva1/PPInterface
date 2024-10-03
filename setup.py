432dc2daa27cd27cf217f0a588097d26037e29ac
nviva1 <n.ivanisenko@gmail.com>
2024-10-03 17:10:38 +0200

    update

--- setup.py
+++ setup.py
@@ -4,30 +4,31 @@ import subprocess
 import os
 import sys
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    
-    def run(self):
-        # First, run the normal install process
-        #install.run(self)
-        self.do_egg_install()        
-        # Get the absolute path to the 'proteinmpnn_wrapper' folder
-        current_dir = os.path.abspath(os.path.dirname(__file__))
-        submodule_dir = os.path.join(current_dir, 'proteinmpnn_wrapper')
-
-        # Check if the 'proteinmpnn_wrapper' directory exists
-        if os.path.isdir(submodule_dir):
-            try:
-                # Change directory to 'proteinmpnn_wrapper'
-                os.chdir(submodule_dir)
-                
-                # Install the package in 'proteinmpnn_wrapper'
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', '.'])
-                print("Successfully installed the proteinmpnn_wrapper package.")
-            except subprocess.CalledProcessError as e:
-                print(f"Error during installation of proteinmpnn_wrapper: {e}")
-        else:
-            print(f"'proteinmpnn_wrapper' directory not found at {submodule_dir}. Skipping installation.")
+#class PostInstallCommand(install):
+#   """Post-installation for installation mode."""
+#   
+#   def run(self):
+#       # First, run the normal install process
+#       self.do_egg_install()       
+#       install.run(self)
+#
+#       # Get the absolute path to the 'proteinmpnn_wrapper' folder
+#       current_dir = os.path.abspath(os.path.dirname(__file__))
+#       submodule_dir = os.path.join(current_dir, 'proteinmpnn_wrapper')
+#
+#       # Check if the 'proteinmpnn_wrapper' directory exists
+#       if os.path.isdir(submodule_dir):
+#           try:
+#               # Change directory to 'proteinmpnn_wrapper'
+#               os.chdir(submodule_dir)
+#               
+#               # Install the package in 'proteinmpnn_wrapper'
+#               subprocess.check_call([sys.executable, '-m', 'pip', 'install', '.'])
+#               print("Successfully installed the proteinmpnn_wrapper package.")
+#           except subprocess.CalledProcessError as e:
+#               print(f"Error during installation of proteinmpnn_wrapper: {e}")
+#       else:
+#           print(f"'proteinmpnn_wrapper' directory not found at {submodule_dir}. Skipping installation.")
 
 # Helper function to read requirements.txt
 def read_requirements():
@@ -42,7 +43,8 @@ setup(
         "biotite==0.40.0",
         "biopandas",
         "biopython",
-        "omegaconf"
+        "omegaconf",
+        "proteinmpnn_wrapper @ git+https://github.com/Croydon-Brixton/proteinmpnn_wrapper.git"
     ],
     #install_requires=read_requirements(),  # Automatically install packages from requirements.txt
     cmdclass={
