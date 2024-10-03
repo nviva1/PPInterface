35dedc48ade9a445df673abd163ef572d95598f3
nviva1 <n.ivanisenko@gmail.com>
2024-10-03 17:25:32 +0200

    update

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "biopandas",
         "biopython",
         "omegaconf",
-        'git+https://github.com/Croydon-Brixton/proteinmpnn_wrapper.git'
+        'proteinmpnn_wrapper git+https://github.com/Croydon-Brixton/proteinmpnn_wrapper'
     ],
     #install_requires=read_requirements(),  # Automatically install packages from requirements.txt
     #cmdclass={#
